c071ba0e2c2faace836d47f3d252c36f7ebb0e0d
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-06 12:38:53 +0530

    Switch Compat Project added to git. This project shows how to use Switch Compat in Android using Java.

---
+++ app/src/main/java/com/example/differentandroidcodes/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.differentandroidcodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SwitchCompat switchCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchCompat = findViewById(R.id.switchCompat);
+
+        switchCompat.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (switchCompat.isChecked()) {
+                Toast.makeText(MainActivity.this, "Switch is on", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Switch is off", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
